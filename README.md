0733aaac7d649a0ad979baa1f2844c3637b2fb7e
Callum Locke <callum.locke@gmail.com>
2015-05-09 17:09:38 +0100

    change of plan

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # asset-reflux
 
-Bundling engine for front end apps.
-
-Docs coming.
+Front end build pipeline tool (experimental/alpha).
 
 [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][depstat-image]][depstat-url]
 
@@ -11,8 +9,8 @@ Docs coming.
 [npm-url]: https://npmjs.org/package/asset-reflux
 [npm-image]: https://img.shields.io/npm/v/asset-reflux.svg?style=flat-square
 
-[travis-url]: http://travis-ci.org/exhibitjs/asset-reflux
-[travis-image]: https://img.shields.io/travis/exhibitjs/asset-reflux.svg?style=flat-square
+[travis-url]: http://travis-ci.org/callumlocke/asset-reflux
+[travis-image]: https://img.shields.io/travis/callumlocke/asset-reflux.svg?style=flat-square
 
-[depstat-url]: https://david-dm.org/exhibitjs/asset-reflux
-[depstat-image]: https://img.shields.io/david/exhibitjs/asset-reflux.svg?style=flat-square
+[depstat-url]: https://david-dm.org/callumlocke/asset-reflux
+[depstat-image]: https://img.shields.io/david/callumlocke/asset-reflux.svg?style=flat-square
--- src/asset-reflux.js
+++ src/asset-reflux.js
@@ -1,22 +1,71 @@
-/*
-  assetReflux()
-  returns a configured Engine instance
- */
-
 'use strict';
 
-const Engine = require('./engine');
+import isArray from 'lodash/lang/isArray';
+import isFunction from 'lodash/lang/isFunction';
+import identity from 'lodash/utility/identity';
+import isObject from 'lodash/lang/isObject';
+import Phase from './phase';
+import VirtualFolder  from 'virtual-folder';
 
-// main exports
-module.exports = function(options) {
-  return new Engine(options);
-};
-module.exports.Engine = Engine;
+const BATCH_RUNNING       = Symbol();
+const FINAL_OUTBOX        = Symbol();
+const INITIAL_INBOX       = Symbol();
+const NUM_PHASES          = Symbol();
+const PHASES              = Symbol();
 
-// also export various utility deps, in case the wrapper lib wants to use them
-module.exports.async = require('async');
-module.exports.Promise = require('bluebird');
-module.exports.chalk = require('chalk');
-module.exports._ = require('lodash');
-module.exports.Args = require('args-js');
-module.exports.XFile = require('x-file');
+
+export default class AssetReflux {
+  constructor(options) {
+    if (options.phases && (
+      !isArray(options.phases) || !options.phases.every(isFunction)
+    )) {
+      throw new TypeError('Expected options.phases to be an array of functions');
+    }
+
+    this[BATCH_RUNNING] = false;
+    this[INITIAL_INBOX] = new VirtualFolder();
+    this[FINAL_OUTBOX] = new VirtualFolder();
+    this[NUM_PHASES] = options.phases ? options.phases.length : 0;
+    this[PHASES] = [];
+
+    for (let i = 0; i < this[NUM_PHASES]; i++) {
+      const fn = options.phases[i];
+      const isFirst = (i === 0);
+      const isLast = (i === options.phases.length - 1);
+
+      const previous = isFirst ? this[INITIAL_INBOX] : this[PHASES][i - 1];
+      const outbox = isLast ? this[FINAL_OUTBOX] : new VirtualFolder();
+
+      this[PHASES][i] = new Phase({previous, outbox, fn});
+    }
+  }
+
+
+  async batch(inFiles) {
+    if (this[BATCH_RUNNING]) {
+      throw new Error('Cannot run two batches at the same time');
+    }
+
+    if (!isArray(inFiles) || !inFiles.every(isObject)) {
+      throw new TypeError('Expected an array of objects');
+    }
+
+    this[BATCH_RUNNING] = true;
+
+    // get an array of actual changes from the first inbox
+    let nextFiles = inFiles.map(file => {
+      return this[INITIAL_INBOX].write(file.filename, file.contents);
+    }).filter(identity);
+
+    for (let phase of this[PHASES]) {
+      nextFiles = await phase.execute(nextFiles);
+      if (!nextFiles || !nextFiles.length) break;
+    }
+
+    this[BATCH_RUNNING] = false;
+    return nextFiles;
+  }
+}
+
+
+AssetReflux.VirtualFolder = VirtualFolder;
